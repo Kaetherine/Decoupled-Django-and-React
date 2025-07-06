80b0543fce0b67ea976ad18f83ee830faa19e871
Kaetherine <kuhle.katherine@googlemail.com>
2025-07-06 12:23:43 +0200

    working, prettier then before

--- mysite_react/src/components/ArticleList.jsx
+++ mysite_react/src/components/ArticleList.jsx
@@ -1,7 +1,8 @@
- import React from 'react'
- import { APIService } from '../APIService'
- 
- function ArticleList(props) {
+import React from 'react'
+import { APIService } from '../APIService'
+import './ArticleList.css'
+
+function ArticleList(props) {
 
     const editBtn = (article) => {
         props.editBtn(article)
@@ -11,29 +12,55 @@
       .then(() => props.deleteBtn(article))
       .catch(error => console.log(error))
      
- }
-   return (
-     <div>
-      {props.articles && props.articles.map((article) => {
-        return (
-          <div key = {article.id} >
-        <h2>{article.title}</h2>
-        <p>{article.description}</p>
-        <div className = "row">
-            <div className = "col-md-1">
-                <button className ="btn btn-primary" onClick = {() => editBtn(article)}>Update</button>
-                </div>
-            <div className = "col">
-                <button onClick = {() => deleteBtn(article)} className ="btn btn-danger">Delete</button>
-            </div></div>
-        <hr></hr>
+    }
+
+    // SVG Icons
+    const EditIcon = () => (
+        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
+        </svg>
+    )
+
+    const DeleteIcon = () => (
+        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/>
+        </svg>
+    )
+
+    return (
+        <div>
+            {props.articles && props.articles.map((article) => {
+                return (
+                    <div key={article.id}>
+                        <h2>{article.title}</h2>
+                        <p>{article.description}</p>
+                        <div className="row">
+                            <div className="col-md-1">
+                                <button 
+                                    className="icon-button edit-button" 
+                                    onClick={() => editBtn(article)}
+                                    title="Edit Article"
+                                >
+                                    <EditIcon />
+                                </button>
+                            </div>
+                            <div className="col">
+                                <button 
+                                    onClick={() => deleteBtn(article)} 
+                                    className="icon-button delete-button"
+                                    title="Delete Article"
+                                >
+                                    <DeleteIcon />
+                                </button>
+                            </div>
+                        </div>
+                        <hr></hr>
+                    </div>
+                )
+            })}
         </div>
-        )
-      })}
-       
-     </div>
-   )
- }
- 
- export default ArticleList
+    )
+}
+
+export default ArticleList
  
